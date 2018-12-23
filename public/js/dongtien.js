96009a484f32294c9972c65e8b6485c8471a2908
NguyenQuocTruong14110425 <14110425@student.hcmute.edu.vn>
2018-12-23 13:57:56 +0700

    v03

--- public/js/dongtien.js
+++ public/js/dongtien.js
@@ -7,11 +7,28 @@ function Shape(x, y, w, h, fill) {
     this.fill = fill || '#AAAAAA';
 }
 
+function Image(img, x,y)
+{
+    this.img = img;
+    this.x = x || 0;
+    this.y = y || 0;
+    this.w = img.width || 1;
+    this.h = img.height || 1;
+}
+
 Shape.prototype.draw = function(ctx) {
     ctx.fillStyle = this.fill;
     ctx.fillRect(this.x, this.y, this.w, this.h);
 }
 Shape.prototype.contains = function(mx, my) {
+    return  (this.x <= mx) && (this.x + this.w >= mx) &&
+        (this.y <= my) && (this.y + this.h >= my);
+}
+
+Image.prototype.draw = function(ctx) {
+    ctx.drawImage(this.img ,this.x, this.y);
+}
+Image.prototype.contains = function(mx, my) {
     return  (this.x <= mx) && (this.x + this.w >= mx) &&
         (this.y <= my) && (this.y + this.h >= my);
 }
@@ -36,6 +53,7 @@ function CanvasState(canvas) {
 
     this.valid = false; // when set to false, the canvas will redraw everything
     this.shapes = [];  // the collection of things to be drawn
+    this.images = [];  // the collection of images
     this.dragging = false; // Keep track of when we are dragging
 
     this.selection = null;
@@ -46,11 +64,10 @@ function CanvasState(canvas) {
 
     canvas.addEventListener('selectstart', function(e) { e.preventDefault(); return false; }, false);
 
-    canvas.addEventListener('mousedown', function(e) {
-        var mouse = myState.getMouse(e);
+    function moveElement(mouse, shapes)
+    {
         var mx = mouse.x;
         var my = mouse.y;
-        var shapes = myState.shapes;
         var l = shapes.length;
         for (var i = l-1; i >= 0; i--) {
             if (shapes[i].contains(mx, my)) {
@@ -69,6 +86,13 @@ function CanvasState(canvas) {
             myState.selection = null;
             myState.valid = false; // Need to clear the old selection border
         }
+    }
+    canvas.addEventListener('mousedown', function(e) {
+        var mouse = myState.getMouse(e);
+        var shapes = myState.shapes;
+        var images = myState.images;
+        moveElement(mouse,shapes);
+        moveElement(mouse,images);
     }, true);
 
     canvas.addEventListener('mousemove', function(e) {
@@ -86,10 +110,10 @@ function CanvasState(canvas) {
         myState.dragging = false;
     }, true);
 
-    canvas.addEventListener('dblclick', function(e) {
-        var mouse = myState.getMouse(e);
-        myState.addShape(new Shape(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0,255,0,.6)'));
-    }, true);
+    // canvas.addEventListener('dblclick', function(e) {
+    //     var mouse = myState.getMouse(e);
+    //     myState.addShape(new Shape(mouse.x - 10, mouse.y - 10, 20, 20, 'rgba(0,255,0,.6)'));
+    // }, true);
 
     this.selectionColor = '#CC0000';
     this.selectionWidth = 2;
@@ -101,9 +125,32 @@ CanvasState.prototype.addShape = function(shape) {
     this.shapes.push(shape);
     this.valid = false;
 }
-
+CanvasState.prototype.addImage = function(image) {
+    this.images.push(image);
+    this.valid = false;
+}
 CanvasState.prototype.clear = function() {
     this.ctx.clearRect(0, 0, this.width, this.height);
+}
+
+function drawElement(ctx,shapes)
+{
+    var l = shapes.length;
+    for (var i = 0; i < l; i++) {
+        var shape = shapes[i];
+        if (shape.x > this.width || shape.y > this.height ||
+            shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
+        shapes[i].draw(ctx);
+    }
+
+    if (this.selection != null) {
+        ctx.strokeStyle = this.selectionColor;
+        ctx.lineWidth = this.selectionWidth;
+        var mySel = this.selection;
+        ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
+    }
+
+    this.valid = true;
 }
 
 CanvasState.prototype.draw = function() {
@@ -111,24 +158,11 @@ CanvasState.prototype.draw = function() {
     if (!this.valid) {
         var ctx = this.ctx;
         var shapes = this.shapes;
+        var images = this.images;
         this.clear();
 
-        var l = shapes.length;
-        for (var i = 0; i < l; i++) {
-            var shape = shapes[i];
-            if (shape.x > this.width || shape.y > this.height ||
-                shape.x + shape.w < 0 || shape.y + shape.h < 0) continue;
-            shapes[i].draw(ctx);
-        }
-
-        if (this.selection != null) {
-            ctx.strokeStyle = this.selectionColor;
-            ctx.lineWidth = this.selectionWidth;
-            var mySel = this.selection;
-            ctx.strokeRect(mySel.x,mySel.y,mySel.w,mySel.h);
-        }
-
-        this.valid = true;
+        drawElement(ctx, shapes);
+        drawElement(ctx, images);
     }
 }
 
@@ -155,12 +189,17 @@ var canvas;
 function init() {
     var s = new CanvasState(document.getElementById('canvas'));
     this.canvas = s;
-    s.addShape(new Shape(40,40,50,50)); // The default is gray
-    s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
-    // Lets make some partially transparent
-    s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
-    s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
-    s.addShape(new Shape(14,20,50,80, 'rgba(255, 153, 255, .6)'));
+    // s.addShape(new Shape(40,40,50,50)); // The default is gray
+    // s.addShape(new Shape(60,140,40,60, 'lightskyblue'));
+    // // Lets make some partially transparent
+    // s.addShape(new Shape(80,150,60,30, 'rgba(127, 255, 212, .5)'));
+    // s.addShape(new Shape(125,80,30,80, 'rgba(245, 222, 179, .7)'));
+    // s.addShape(new Shape(14,20,50,80, 'rgba(255, 153, 255, .6)'));
+    var img = document.getElementById("scream");
+    s.addImage(new Image(img,50,80));
+    s.addImage(new Image(img,150,200));
+    s.addImage(new Image(img,100,300));
+    s.addImage(new Image(img,10,250));
 }
 
 function createShape(option) {
@@ -171,6 +210,13 @@ function createShape(option) {
         case 'rectangle':
             this.canvas.addShape(new Shape(100,200,100,50, 'rgba(255, 153, 255, .6)'));
             break;
+        case 'socola':
+            var img = document.getElementById("scream");
+            this.canvas.addImage(new Image(img,500,250));
+            break;
     }
+function createImage() {
+    
+}
 
 }
--- resources/views/client/home/socola.blade.php
+++ resources/views/client/home/socola.blade.php
@@ -6,12 +6,12 @@
     <div class="body-bg row">
         <div class="col-md-2">
             <div class="left-sidebar ">
-                    <img class="big-size-kkc" src="{{URL::asset('/public/images/valetine/khinhkhicau.png')}}"/>
-                    <img class="small-size-kkc" src="{{URL::asset('/public/images/valetine/khinhkhicau.png')}}"/>
-                    <img class="may-01" src="{{URL::asset('/public/images/valetine/may.png')}}"/>
-                    <img class="may-02" src="{{URL::asset('/public/images/valetine/may.png')}}"/>
-                    <img class="hoa-01" src="{{URL::asset('/public/images/valetine/hoa1.png')}}"/>
-                    <img class="hoa-02" src="{{URL::asset('/public/images/valetine/hoa1.png')}}"/>
+                    <img class="big-size-kkc" src="{{URL::asset('/public/images/valentine/khinhkhicau.png')}}"/>
+                    <img class="small-size-kkc" src="{{URL::asset('/public/images/valentine/khinhkhicau.png')}}"/>
+                    <img class="may-01" src="{{URL::asset('/public/images/valentine/may.png')}}"/>
+                    <img class="may-02" src="{{URL::asset('/public/images/valentine/may.png')}}"/>
+                    <img class="hoa-01" src="{{URL::asset('/public/images/valentine/hoa1.png')}}"/>
+                    <img class="hoa-02" src="{{URL::asset('/public/images/valentine/hoa1.png')}}"/>
             </div>
         </div>
         <div class="col-sm-12 col-md-8">
@@ -24,23 +24,24 @@
                     <div class="col-sm-12 col-md-4">
                         <div class="list-item-socola">
                             <button onclick="init()" class="btn btn-success">run</button>
-                            <button onclick="createShape('square')" class="btn btn-info">create element</button>
+                            <button onclick="createShape('socola')" class="btn btn-info">create element</button>
+                            <img id="scream" src="{{URL::asset('/public/images/valentine/socola.png')}}">
                         </div>
                     </div>
                 </div>
         </div>
         <div class="col-md-2">
             <div class="right-sidebar ">
-                <img class="big-size-kkc" src="{{URL::asset('/public/images/valetine/khinhkhicau.png')}}"/>
-                <img class="small-size-kkc" src="{{URL::asset('/public/images/valetine/khinhkhicau.png')}}"/>
-                <img class="may-01" src="{{URL::asset('/public/images/valetine/may.png')}}"/>
-                <img class="may-02" src="{{URL::asset('/public/images/valetine/may.png')}}"/>
-                <img class="hoa-01" src="{{URL::asset('/public/images/valetine/hoa1.png')}}"/>
-                <img class="hoa-02" src="{{URL::asset('/public/images/valetine/hoa1.png')}}"/>
+                <img class="big-size-kkc" src="{{URL::asset('/public/images/valentine/khinhkhicau.png')}}"/>
+                <img class="small-size-kkc" src="{{URL::asset('/public/images/valentine/khinhkhicau.png')}}"/>
+                <img class="may-01" src="{{URL::asset('/public/images/valentine/may.png')}}"/>
+                <img class="may-02" src="{{URL::asset('/public/images/valentine/may.png')}}"/>
+                <img class="hoa-01" src="{{URL::asset('/public/images/valentine/hoa1.png')}}"/>
+                <img class="hoa-02" src="{{URL::asset('/public/images/valentine/hoa1.png')}}"/>
             </div>
         </div>
     </div>
-    <img class="may-2" src="{{URL::asset('/public/images/valetine/may2.png')}}"/>
+    <img class="may-2" src="{{URL::asset('/public/images/valentine/may2.png')}}"/>
 @endsection
 @section('script-footer')
     <script src="{{URL::asset('public/js/dongtien.js')}}"></script>
